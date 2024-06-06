eda2bd257ebbf524d5632ecbfb8c778353d1e8aa
egorsv1985 <egorvs2009@gmail.com>
2024-06-06 17:02:48 +0300

    fix

--- public_html/bitrix/templates/bestmaket/components/bitrix/news.list/number/template.php
+++ public_html/bitrix/templates/bestmaket/components/bitrix/news.list/number/template.php
@@ -1,4 +1,4 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 /** @var array $arParams */
 /** @var array $arResult */
 /** @global CMain $APPLICATION */
@@ -11,26 +11,27 @@
 /** @var string $componentPath */
 /** @var CBitrixComponent $component */
 $this->setFrameMode(true);
+// print_r($arResult);
 ?>
-	  		<div class="number-items">
-<?foreach($arResult["ITEMS"] as $arItem):?>
-	<?
-	$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
-	$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+<div class="number-items">
+	<? foreach ($arResult["ITEMS"] as $arItem) : ?>
+		<?
+		$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+		$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
 		//print_r($arItem);
-	?>
-	  			<div class="number-item <?=$arItem['PROPERTIES']['SIZE']['VALUE_XML_ID'];?> <?=$arItem['PROPERTIES']['COLOR']['VALUE_XML_ID'];?>" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
-	<?if ($arItem['PREVIEW_PICTURE']['SRC']):?>
-	  				<div class="number-contaner" style="background-image:url('<?=$arItem['PREVIEW_PICTURE']['SRC']?>')"></div>
-	<?else:?>
-	  				<div class="number-contaner">
-	  					<div class="number"><?=$arItem['PROPERTIES']['NUMBER']['VALUE'];?></div>
-	  					<div class="name">
-								<?=$arItem['~NAME'];?>
-							</div>
-						</div>
-	<?endif;?>
-	  			</div>
-<?endforeach;?>
-	  			<div class="clear"></div>
-	  		</div>
+		?>
+		<div class="number-item <?= $arItem['PROPERTIES']['SIZE']['VALUE_XML_ID']; ?> <?= $arItem['PROPERTIES']['COLOR']['VALUE_XML_ID']; ?>" id="<?= $this->GetEditAreaId($arItem['ID']); ?>">
+			<? if ($arItem['PREVIEW_PICTURE']['SRC']) : ?>
+				<div class="number-contaner" style="background-image:url('<?= $arItem['PREVIEW_PICTURE']['SRC'] ?>')"></div>
+			<? else : ?>
+				<div class="number-contaner">
+					<div class="number"><?= $arItem['PROPERTIES']['NUMBER']['VALUE']; ?></div>
+					<div class="name">
+						<?= $arItem['~NAME']; ?>
+					</div>
+				</div>
+			<? endif; ?>
+		</div>
+	<? endforeach; ?>
+	<div class="clear"></div>
+</div>
