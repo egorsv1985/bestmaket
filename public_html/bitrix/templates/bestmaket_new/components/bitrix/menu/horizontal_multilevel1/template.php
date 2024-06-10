56cc7b40b74b82b9348c2cbd5e1aaa9e39993844
egorsv1985 <egorvs2009@gmail.com>
2024-06-10 15:52:22 +0300

    added header fix

---
+++ public_html/bitrix/templates/bestmaket_new/components/bitrix/menu/horizontal_multilevel1/template.php
@@ -0,0 +1,46 @@
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
+
+<? if (!empty($arResult)) : ?>
+	<ul class="d-flex justify-content-between gap-4">
+
+<?
+	$previousLevel = 0;
+	foreach ($arResult as $arItem) : ?>
+
+	<? if ($previousLevel && $arItem["DEPTH_LEVEL"] < $previousLevel) : ?>
+		<?= str_repeat("</ul ></li>", ($previousLevel - $arItem["DEPTH_LEVEL"])); ?>
+	<? endif ?>
+
+	<? if ($arItem["IS_PARENT"]) : ?>
+
+		<? if ($arItem["DEPTH_LEVEL"] == 1) : ?>
+			<li><a href=" <?= $arItem["LINK"] ?>" class="text-nowrap <? if ($arItem["SELECTED"]) : ?>contactsActive submenu-link<? else : ?><? endif ?>"><?= $arItem["TEXT"] ?></a>
+		<ul class="submenu">
+		<? else : ?>
+			<li<? if ($arItem["SELECTED"]) : ?> class="contactsActive" <? endif ?>><a href="<?= $arItem["LINK"] ?>" class="text-nowrap contactsActive"><?= $arItem["TEXT"] ?></a>
+				<ul>
+				<? endif ?>
+			<? else : ?>
+				<? if ($arItem["PERMISSION"] > "D") : ?>
+					<? if ($arItem["DEPTH_LEVEL"] == 1) : ?>
+						<li><a href="<?= $arItem["LINK"] ?>" class="text-nowrap <? if ($arItem["SELECTED"]) : ?>contactsActive<? else : ?><? endif ?>"><?= $arItem["TEXT"] ?></a></li>
+					<? else : ?>
+						<li<? if ($arItem["SELECTED"]) : ?> class="text-nowrap contactsActive" <? endif ?>><a class="asd selected" href="<?= $arItem["LINK"] ?>"><?= $arItem["TEXT"] ?></a></li>
+						<? endif ?>
+					<? else : ?>
+						<? if ($arItem["DEPTH_LEVEL"] == 1) : ?>
+							<li><a href="" class="text-nowrap <? if ($arItem["SELECTED"]) : ?>selected<? else : ?><? endif ?>" title="<?= GetMessage("MENU_ITEM_ACCESS_DENIED") ?>"><?= $arItem["TEXT"] ?></a></li>
+						<? else : ?>
+							<li><a href="" class="text-nowrap denied" title="<?= GetMessage("MENU_ITEM_ACCESS_DENIED") ?>"><?= $arItem["TEXT"] ?></a></li>
+						<? endif ?>
+					<? endif ?>
+				<? endif ?>
+				<? $previousLevel = $arItem["DEPTH_LEVEL"]; ?>
+
+			<? endforeach ?>
+			<? if ($previousLevel > 1) : //close last item tags
+			?>
+				<?= str_repeat("</ul></li>", ($previousLevel - 1)); ?>
+			<? endif ?>
+				</ul>
+			<? endif ?>
---
+++ public_html/includes/top-phone-new.php
@@ -0,0 +1,10 @@
+<div class="phone ">
+	<div class="d-flex justify-content-between gap-4">
+		<a href="tel:+78126470637" class="phone-link callibri_phone_msk text-nowrap">+7 (812) 647-06-37</a>
+		<a href="tel:88007778504" class="phone-link callibri_phone_800 text-nowrap"> 8 (800) 777-85-04</a>
+	</div>
+	<div class="open-popup text-end">
+		<!--a href="#popupOrder" class="fancy">заказать звонок</a-->
+		<a href="mailto:zakaz2@bestmaket.ru?subject=Заявка%20с%20bestmaket.ru" class="js-promo-email" target="_blank">zakaz2@bestmaket.ru</a>
+	</div>
+</div>
