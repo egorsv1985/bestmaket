1ebf15b0bd16124b28996c0f7881d575c84892e2
egorsv1985 <egorvs2009@gmail.com>
2024-06-13 16:51:12 +0300

    fix

--- public_html/bitrix/templates/bestmaket_new/footer.php
+++ public_html/bitrix/templates/bestmaket_new/footer.php
@@ -99,15 +99,198 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
 			?>
 		<? endif; ?>
 	<? endif; ?>
-	<div class="map">
-		<?
-		$APPLICATION->IncludeFile("/includes/map.php", array(), array(
-			'NAME' => 'карту',
-			'MODE' => 'php'
-		));
-		?>
+	<section class="main-contacts">
+		<div class="container">
+			<div class="d-flex justify-content-between">
+				<div class="main-contacts__box">
+
+					<div class="large-title">
+						Контакты
+					</div>
+					<div class="contact-block">
+						<div class="title">Офис в Москве</div>
+						<div class="address">
+							г. Москва<br>
+							Каширское шоссе, д. 24
+						</div>
+						<div class="phone">
+							<a href="tel:+74956470637" class="">+7 (495) 647-06-37</a>
+						</div>
+						<div class="address">
+							Пн-Пт. с 10.00 до 19.00
+						</div>
+					</div>
+				</div>
+
+
+				<div class="form-main contact">
+					<? $APPLICATION->IncludeComponent(
+						"tkk:infoportal.element.add.form",
+						"form-small",
+						array(
+							"COMPONENT_TEMPLATE" => "form-small",
+							"CUSTOM_TITLE_DATE_ACTIVE_FROM" => "",
+							"CUSTOM_TITLE_DATE_ACTIVE_TO" => "",
+							"CUSTOM_TITLE_DETAIL_PICTURE" => "",
+							"CUSTOM_TITLE_DETAIL_TEXT" => "",
+							"CUSTOM_TITLE_IBLOCK_SECTION" => "",
+							"CUSTOM_TITLE_NAME" => "Ваше имя",
+							"CUSTOM_TITLE_PREVIEW_PICTURE" => "",
+							"CUSTOM_TITLE_PREVIEW_TEXT" => "Сообщение",
+							"CUSTOM_TITLE_TAGS" => "",
+							"DEFAULT_INPUT_SIZE" => "30",
+							"DETAIL_TEXT_USE_HTML_EDITOR" => "N",
+							"ELEMENT_ASSOC" => "CREATED_BY",
+							"FORM_ID" => "topMainForm",
+							"GROUPS" => array(
+								0 => "2",
+							),
+							"IBLOCK_ID" => "2",
+							"IBLOCK_TYPE" => "system",
+							"LEVEL_LAST" => "Y",
+							"LIST_URL" => "",
+							"MAX_FILE_SIZE" => "0",
+							"MAX_LEVELS" => "100000",
+							"MAX_USER_ENTRIES" => "100000",
+							"PREVIEW_TEXT_USE_HTML_EDITOR" => "N",
+							"PROPERTY_CODES" => array(
+								0 => "1",
+								1 => "2",
+								2 => "8",
+								3 => "NAME",
+							),
+							"PROPERTY_CODES_REQUIRED" => array(
+								0 => "1",
+								1 => "8",
+								2 => "NAME",
+							),
+							"RESIZE_IMAGES" => "N",
+							"SEF_MODE" => "N",
+							"STATUS" => "ANY",
+							"STATUS_NEW" => "N",
+							"USER_MESSAGE_ADD" => "",
+							"USER_MESSAGE_EDIT" => "",
+							"USE_CAPTCHA" => "N"
+						),
+						false
+					); ?>
+
+				</div>
+			</div>
+		</div>
+
+	</section>
+	<div class="maps">
+		<script data-skip-moving="true" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA7IG-mXqYnNwnQDFvhVi17zNT_zsTrBxY&callback=initMap" async defer></script>
+		<script data-skip-moving="true">
+			function initMap() {
+				var styledMapType = new google.maps.StyledMapType(
+					[{
+							"featureType": "administrative.land_parcel",
+							"stylers": [{
+									"color": "#2c6fb3"
+								},
+								{
+									"visibility": "simplified"
+								}
+							]
+						},
+						{
+							"featureType": "landscape.man_made",
+							"elementType": "geometry.fill",
+							"stylers": [{
+								"color": "#e6eff9"
+							}]
+						},
+						{
+							"featureType": "landscape.man_made",
+							"elementType": "geometry.stroke",
+							"stylers": [{
+								"color": "#afcdec"
+							}]
+						},
+						{
+							"featureType": "poi.park",
+							"elementType": "geometry.fill",
+							"stylers": [{
+								"color": "#c6dbf1"
+							}]
+						},
+						{
+							"featureType": "poi.park",
+							"elementType": "labels.text.fill",
+							"stylers": [{
+								"color": "#2b6cae"
+							}]
+						},
+						{
+							"featureType": "water",
+							"stylers": [{
+								"color": "#bcd5ef"
+							}]
+						}
+					], {
+						name: 'Стиль'
+					});
+
+
+				var map1 = new google.maps.Map(document.getElementById('map1'), {
+					center: {
+						lat: 59.94821017035078,
+						lng: 30.27223597015389
+					},
+					zoom: 17,
+					mapTypeControlOptions: {
+						mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain', 'styled_map']
+					}
+				});
+
+				var coordinates = {
+					lat: 59.94821017035078,
+					lng: 30.27223597015389
+				};
+				var popupContent = '<p class="map-balloon">г.Санкт-Петербург,<br>7-ая линия В.О., д.78</p>';
+				var imgMarker = '<?= SITE_TEMPLATE_PATH; ?>/images/marker.png';
+				marker1 = new google.maps.Marker({
+					position: coordinates,
+					map: map1,
+					icon: imgMarker,
+				});
+				infowindow1 = new google.maps.InfoWindow({
+					content: popupContent
+				});
+
+				marker1.addListener('click', function() {
+					infowindow1.open(map1, marker1);
+				});
+
+
+
+
+
+
+
+				map1.mapTypes.set('styled_map', styledMapType);
+				map1.setMapTypeId('styled_map');
+
+			}
+		</script>
+		<div class="left">
+			<?
+			$APPLICATION->IncludeFile("/includes/map-left.php", array(), array(
+				'NAME' => 'текст',
+				'MODE' => 'html',
+				'SHOW_BORDER' => false
+			));
+			?>
+		</div>
+
 	</div>
-	
+
+
+
+
+
 	<div class="contact-text">
 		<?
 		$APPLICATION->IncludeFile($APPLICATION->GetCurPage() . "/contact-text.php", array(), array(
@@ -115,62 +298,6 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
 			'MODE' => 'text'
 		));
 		?>
-	</div>
-	<div class="form-main contact">
-		<? $APPLICATION->IncludeComponent(
-			"tkk:infoportal.element.add.form",
-			"form-large",
-			array(
-				"FORM_ID" => 'contactForm',
-				"TITLE" => "Написать сообщение",
-				"CONTACT" => "Y",
-				"CUSTOM_TITLE_DATE_ACTIVE_FROM" => "",	// * дата начала *
-				"CUSTOM_TITLE_DATE_ACTIVE_TO" => "",	// * дата завершения *
-				"CUSTOM_TITLE_DETAIL_PICTURE" => "",	// * подробная картинка *
-				"CUSTOM_TITLE_DETAIL_TEXT" => "",	// * подробный текст *
-				"CUSTOM_TITLE_IBLOCK_SECTION" => "",	// * раздел инфоблока *
-				"CUSTOM_TITLE_NAME" => "Ваше имя",	// * наименование *
-				"CUSTOM_TITLE_PREVIEW_PICTURE" => "",	// * картинка анонса *
-				"CUSTOM_TITLE_PREVIEW_TEXT" => "Сообщение",	// * текст анонса *
-				"CUSTOM_TITLE_TAGS" => "",	// * теги *
-				"DEFAULT_INPUT_SIZE" => "30",	// Размер полей ввода
-				"DETAIL_TEXT_USE_HTML_EDITOR" => "N",	// Использовать визуальный редактор для редактирования подробного текста
-				"ELEMENT_ASSOC" => "CREATED_BY",	// Привязка к пользователю
-				"GROUPS" => array(	// Группы пользователей, имеющие право на добавление/редактирование
-					0 => "2",
-				),
-				"IBLOCK_ID" => "2",	// Инфо-блок
-				"IBLOCK_TYPE" => "system",	// Тип инфо-блока
-				"LEVEL_LAST" => "Y",	// Разрешить добавление только на последний уровень рубрикатора
-				"LIST_URL" => "",	// Страница со списком своих элементов
-				"MAX_FILE_SIZE" => "0",	// Максимальный размер загружаемых файлов, байт (0 - не ограничивать)
-				"MAX_LEVELS" => "100000",	// Ограничить кол-во рубрик, в которые можно добавлять элемент
-				"MAX_USER_ENTRIES" => "100000",	// Ограничить кол-во элементов для одного пользователя
-				"PREVIEW_TEXT_USE_HTML_EDITOR" => "N",	// Использовать визуальный редактор для редактирования текста анонса
-				"PROPERTY_CODES" => array(	// Свойства, выводимые на редактирование
-					0 => "1",
-					1 => "2",
-					2 => "NAME",
-					3 => "PREVIEW_TEXT",
-					4 => "8",
-				),
-				"PROPERTY_CODES_REQUIRED" => array(	// Свойства, обязательные для заполнения
-					0 => "1",
-					1 => "8",
-					2 => "NAME",
-				),
-				"RESIZE_IMAGES" => "N",	// Использовать настройки инфоблока для обработки изображений
-				"SEF_MODE" => "N",	// Включить поддержку ЧПУ
-				"STATUS" => "ANY",	// Редактирование возможно
-				"STATUS_NEW" => "N",	// Деактивировать элемент
-				"USER_MESSAGE_ADD" => "",	// Сообщение об успешном добавлении
-				"USER_MESSAGE_EDIT" => "",	// Сообщение об успешном сохранении
-				"USE_CAPTCHA" => "N",	// Использовать CAPTCHA
-				"COMPONENT_TEMPLATE" => ".default"
-			),
-			false
-		); ?>
-
 	</div>
 	<div class="footer">
 		<div class="contaner">
---
+++ public_html/includes/contact-right.php
@@ -0,0 +1,11 @@
+<div class="title">Офис в Москве</div>
+<div class="address">
+	г. Москва<br>
+	Каширское шоссе, д. 24 
+</div>
+<div class="phone">
+	<a href="tel:+74956470637" class="">+7 (495) 647-06-37</a>
+</div>
+<div class="address">
+    Пн-Пт. с 10.00 до 19.00
+</div>
