949de2bc2955acb425b7e08bc880b62aeb5b076c
egorsv1985 <egorvs2009@gmail.com>
2024-06-18 12:02:54 +0300

    added mob-menu for desktop

--- public_html/bitrix/templates/bestmaket_new/components/bitrix/menu/horizontal_multilevel_mob/template.php
+++ public_html/bitrix/templates/bestmaket_new/components/bitrix/menu/horizontal_multilevel_mob/template.php
@@ -5,7 +5,18 @@
 
 		<?
 		$previousLevel = 0;
-		foreach ($arResult as $arItem) : ?>
+		$noSubmenuCount = 0; // Счетчик для элементов без подменю
+		foreach ($arResult as $key => $arItem) :
+			$liClass = '';
+			if ($key == 4) { // Второй элемент имеет индекс 1, так как индексация начинается с 0
+				$liClass = 'second-item';
+			} elseif (!$arItem["IS_PARENT"] && $arItem["DEPTH_LEVEL"] == 1) {
+				$noSubmenuCount++;
+				if ($noSubmenuCount > 1) { // Пропускаем первый элемент без подменю
+					$liClass = 'no-submenu';
+				}
+			}
+		?>
 
 			<? if ($previousLevel && $arItem["DEPTH_LEVEL"] < $previousLevel) : ?>
 				<?= str_repeat("</ul ></li>", ($previousLevel - $arItem["DEPTH_LEVEL"])); ?>
@@ -14,7 +25,8 @@
 			<? if ($arItem["IS_PARENT"]) : ?>
 
 				<? if ($arItem["DEPTH_LEVEL"] == 1) : ?>
-					<li><a href=" <?= $arItem["LINK"] ?>" class="text-nowrap <? if ($arItem["SELECTED"]) : ?>contactsActive submenu-link<? else : ?><? endif ?>"><?= $arItem["TEXT"] ?></a>
+					<li class="<?= $liClass ?>"><a href="<?= $arItem["LINK"] ?>" class="text-nowrap <? if ($arItem["SELECTED"]) : ?>contactsActive submenu-link<? else : ?><? endif ?>"><?= $arItem["TEXT"] ?></a>
+
 						<ul class="submenu">
 						<? else : ?>
 							<li<? if ($arItem["SELECTED"]) : ?> class="contactsActive" <? endif ?>><a href="<?= $arItem["LINK"] ?>" class="text-nowrap contactsActive"><?= $arItem["TEXT"] ?></a>
@@ -23,16 +35,16 @@
 							<? else : ?>
 								<? if ($arItem["PERMISSION"] > "D") : ?>
 									<? if ($arItem["DEPTH_LEVEL"] == 1) : ?>
-										<li><a href="<?= $arItem["LINK"] ?>" class="text-nowrap <? if ($arItem["SELECTED"]) : ?>contactsActive<? else : ?><? endif ?>"><?= $arItem["TEXT"] ?></a></li>
+										<li class="<?= $liClass ?>"><a href="<?= $arItem["LINK"] ?>" class="text-nowrap <? if ($arItem["SELECTED"]) : ?>contactsActive<? else : ?><? endif ?>"><?= $arItem["TEXT"] ?></a></li>
 									<? else : ?>
 										<li<? if ($arItem["SELECTED"]) : ?> class="text-nowrap contactsActive" <? endif ?>><a class="asd selected" href="<?= $arItem["LINK"] ?>"><?= $arItem["TEXT"] ?></a>
 					</li>
 				<? endif ?>
 			<? else : ?>
 				<? if ($arItem["DEPTH_LEVEL"] == 1) : ?>
-					<li><a href="" class="text-nowrap <? if ($arItem["SELECTED"]) : ?>selected<? else : ?><? endif ?>" title="<?= GetMessage("MENU_ITEM_ACCESS_DENIED") ?>"><?= $arItem["TEXT"] ?></a></li>
+					<li class="<?= $liClass ?>"><a href="" class="text-nowrap <? if ($arItem["SELECTED"]) : ?>selected<? else : ?><? endif ?>" title="<?= GetMessage("MENU_ITEM_ACCESS_DENIED") ?>"><?= $arItem["TEXT"] ?></a></li>
 				<? else : ?>
-					<li><a href="" class="text-nowrap denied" title="<?= GetMessage("MENU_ITEM_ACCESS_DENIED") ?>"><?= $arItem["TEXT"] ?></a></li>
+					<li class="<?= $liClass ?>"><a href="" class="text-nowrap denied" title="<?= GetMessage("MENU_ITEM_ACCESS_DENIED") ?>"><?= $arItem["TEXT"] ?></a></li>
 				<? endif ?>
 			<? endif ?>
 		<? endif ?>
